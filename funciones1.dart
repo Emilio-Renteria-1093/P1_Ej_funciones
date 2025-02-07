6988f146afceebcc8e9bb21d85063291b55b88f8
Emilio-Renteria-1093 <164533893+Emilio-Renteria-1093@users.noreply.github.com>
2025-02-07 16:47:46 +0000

    mi primera vez

---
+++ funciones1.dart
@@ -0,0 +1,22 @@
+//funcion que no devuelve valor
+
+//funcion saludar
+void saludar() {
+  print("Llamando a la funcion saludar");
+}
+
+void escribirNombre() {
+  print("Emilio Renteria");
+}
+
+void datospersonales() {
+  print("Emilio Renteria 22308051281093");
+}
+void main() {
+  //Emilio Renteria 22308051281093
+  //Llamar a la funcion saludar
+
+  datospersonales();
+  escribirNombre();
+  saludar();
+}
